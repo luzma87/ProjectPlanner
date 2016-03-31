1c2b9f287b83f65723480292a894ea0e829acd6c
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2016-03-31 07:23:48 +0530

    <Assignment solution> Saving the data in the database.

--- app/src/main/java/android/bootcamp/projectplanner/storage/DataStorageManager.java
+++ app/src/main/java/android/bootcamp/projectplanner/storage/DataStorageManager.java
@@ -1,9 +1,11 @@
 package android.bootcamp.projectplanner.storage;
 
 import android.bootcamp.projectplanner.model.Plan;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import static android.provider.BaseColumns._ID;
 
 
 public class DataStorageManager extends SQLiteOpenHelper {
@@ -12,13 +14,18 @@ public class DataStorageManager extends SQLiteOpenHelper {
   private static final String VELOCITY_COL = "velocity";
   private static final String ITERATIONS_COL = "iterations";
   private static final String DB_NAME = "plan.db";
+  private static final String DATABASE_CREATE = "CREATE TABLE " + TABLE_NAME + " (" + _ID
+      + " INTEGER PRIMARY KEY AUTOINCREMENT, " + POINTS_COL
+      + " INTEGER NOT NULL ," + VELOCITY_COL + " INTEGER NOT NULL ," + ITERATIONS_COL + " INTEGER NOT NULL );";
+
 
   public DataStorageManager(Context context) {
     super(context, DB_NAME, null, 1);
   }
 
   @Override
-  public void onCreate(SQLiteDatabase db) { //create your database
+  public void onCreate(SQLiteDatabase db) {
+    db.execSQL(DATABASE_CREATE);
   }
 
   @Override
@@ -26,7 +33,15 @@ public class DataStorageManager extends SQLiteOpenHelper {
   }
 
 
-  public void store(Plan plan) { //insert into your database
+  public void store(Plan plan) {
+    SQLiteDatabase db = getWritableDatabase();
+
+    ContentValues values = new ContentValues();
+    values.put(POINTS_COL, plan.getPoints());
+    values.put(VELOCITY_COL, plan.getVelocity());
+    values.put(ITERATIONS_COL, plan.getNumOfIterations());
+
+    db.insert(TABLE_NAME, null, values);
   }
 
 }
